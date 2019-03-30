5871c6992d5b6e4d6e7776b5be3a85d4b5c1f25c
Kevin Quinn <kpquinn2@wisc.edu>
2019-03-30 12:56:02 -0500

    basic version of script finished

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Information on using Pipfile: [Pipenv](https://github.com/pypa/pipenv).
 
 Alternative methods of using Python work as well, read the Pipfile for necessary packages and Python version and use your preferred tools.
+
+TODO:
+* Handle AND/OR special cases in query
+* Add error handling for file writing issues
+* Add error handling for request issues
+* Add load-balancing for the many requests we have to make (Optional)
+* better file naming convention
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import argparse
 import requests
 from bs4 import BeautifulSoup
+from subjects import subject_list
+import random
 
+global_cookie = ""
 
 # login uses saml, which is a huge suck to do in a script i guess
 # able to login with NetID and get cookie or have user do it
@@ -14,65 +18,130 @@ from bs4 import BeautifulSoup
 old = "http://www.booksinprint.com.ezproxy.library.wisc.edu/Search/Results?q=subject-all%3D[Conduct%20of%20Life]%20%7B%7B(publisher-date%3D[2000-01-01~To~2000-12-31])%7D%7D&ast=se"
 # print(parse_qs("q=subject-all%3D[Conduct%20of%20Life]%20%7B%7B(publisher-date%3D[2000-01-01~To~2000-12-31])%7D%7D&ast=se"))
 
-# # using httpie it sends me to netid login page, need to find a way around that
-# cookies = {
-#     #".ASPXAUTH": "45BBC1B99A6830F12B6567AEB47A74A615516188F849A7FBBC1D9CED8C5BBCB2B221CB8BE0698B92D9BE28C494E62342EBBA22E0FC811667B80613B772B5015F691D9CEC8904EFF0FB2C2B8259171199F1106BFF2F856E29366FEB6B3A1A81E167CDD4B86FACFFA44E94AC2A4F6216B2DC3F7C4FDBBC6B08D763CA46FC6E28256B139D2C0200686BA66F274E5DD5A5E6FD7610B98016D3362F260B59D997A9C9EB8A55CE384E1944DD4B4B1195F007A975A7BC0A64FDD45EED03216E3B1ED830C1E1E27B3E81A592E5C69BCE96B1A79D",
-#     #"ezproxy": "YvTB0BRT4uq2sqB"
-# }
-
+# Speed up Scraping
+#https://stackoverflow.com/questions/23460169/how-to-speed-up-web-scraping-in-python
+# also grab that book on web scraping we have saved somewhere
 
 def find_result_count(page):
-# TODO: fat file, so it takes a long time for BeautifulSoup to load it in,
-# if this needs to be run a lot would want to find a way to minimize time/downloads
-    with open("divNoresults.html", "rb") as f:
-        soup = BeautifulSoup(f, "html.parser")
-        no_results_div = soup.find("div", {"id": "divNoresults"})
-        if no_results_div:
-            print("Got no results page")
-            total = 0
-        else:
-            #example:  <span id="resultsCount">Showing 1- 25 of 889</span>
-            result_span = soup.find("span", {"id": "resultsCount"})
-            print("___________________")
-            print(result_span)
-            words = result_span.text
-            total = words[-1]
-        
-        print("Total: %d" %total)
-        return total
+    total = 0
+    # TODO: fat file, so it takes a long time for BeautifulSoup to load it in,
+    # if this needs to be run a lot would want to find a way to minimize time/downloads  
+    soup = BeautifulSoup(page, "html.parser")
+    no_results_div = soup.find("div", {"id": "divNoresults"})
+    if no_results_div:
+        print("... no results found")
+        total = 0
+    else:
+        # example:  <span id="resultsCount">Showing 1- 25 of 889</span>
+        result_span = soup.find("span", {"id": "resultsCount"})
+        words = result_span.text.split(" ")
+        total = int(words[-1])
+    
+    return total
 
 
 def acquire_page(cookie, query):
     base_url = "http://www.booksinprint.com.ezproxy.library.wisc.edu"
     path = "/Search/Results"
-    # TODO: find out if this is the cookie we need
+    # TODO: this cookie works, but is it actually all we need?
     cookies = {
         "ezproxy": cookie
     }
-    resp = requests.get(base_url + path, params=query, cookies=cookies)
+    try:
+        resp = requests.get(base_url + path, params=query, cookies=cookies)
+    except requests.RequestException:
+        print('[!] failed while making request')
+        return None
+
     if resp.status_code > 300:
         print("[!] failed, got resp %d" % resp.status_code)
         return None
 
+    print("... received page")
     return resp.content
 
 
-def main():
+def get_subject_total_for_year(name, year):
+    print("[*] getting total for %s:%s" % (name, year))
+    # date range for each subject is year-01-01 to year-12-31
+    date_range = "%s-01-01~To~%s-12-31" %(year, year)
 
     # TODO: have to be able to handle AND queries and other special symbols
     #  ['subject-all=[Children] AND subject-all=[Management] '],  same for OR
-    #
-    # query = {
-    #     "q": "subject-all-[%s] {{(publisher-date=[%s~To~%s])}}" %(subject, start_date, end_date),
-    #     "ast": "se"
-    # }
+    query = {
+        "q": ["subject-all=[%s] {{(publisher-date=[%s])}}" %(name, date_range)],
+        "ast": ["pr"]
+    }
 
-    # page = acquire_page(cookie, query)
-    # if not page:
-    #     raise SystemExit
+    if global_cookie is "":
+        print("[!] cookie isn't set before trying to make requests")
+        raise SystemExit
 
-    find_result_count(None)
+    page = acquire_page(global_cookie, query)
+    if not page:
+        raise SystemExit
+    
+    return find_result_count(page)
 
+def write_csv_headers(f):
+    f.write("year,Adult Children,Alcoholics Anonymous,Codependence\n")
+
+
+def main():
+    # 1957 - 2018
+    # queries per subject 61, so we have about 77*61=4600 requests to make
+    # each row in sheet is a year, so 77 requests per year
+    start_year = -1
+    num_years = 0
+    file_name = ""
+    global global_cookie
+
+    # REMOVE ME rIJ49E0fdjudv7G
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", type=int, help="Year to start from.")
+    parser.add_argument("number", type=int, default=10, help="Number of years to run (including start year), max of 100.")
+    parser.add_argument("--file", help="CSV file to create or append to.")
+    args = parser.parse_args()
+
+    if not args.file:
+        needs_headers = True
+        file_name = "bowker-data%d.csv" % random.randint(1,500)
+        print("Creating file %s" % file_name)
+    else:
+        file_name = args.file
+        needs_headers = False
+
+    if not (1957 <= args.year <= 2025):
+        print("%d not in range 1957-2025")
+        return
+    else:
+        start_year = args.year
+
+    if not (1 <= args.number <= 100):
+        print("%d not in range 1-100")
+        return
+    else:
+        num_years = args.number
+
+    global_cookie = input("Please provide the cookie necessary for requests: ")
+
+    
+    with open(file_name, "a") as f:
+        if needs_headers:
+            write_csv_headers(f)
+
+        # num-years -1 because we include start
+        print("Running for year range %d to %d" % (start_year, start_year + num_years - 1))
+        # no -1 because range is non-inclusive
+        for year in range(start_year, start_year + num_years):
+            f.write("%d" %year)
+            for subject in [ "Adult Children","Alcoholics Anonymous", "Codependence"]:
+                total = get_subject_total_for_year(subject, year)
+                f.write(",%d"%total)
+            f.write("\n")
 
 if __name__ == "__main__":
     main()
+
+
+# have 77 categories with 61 requests for each, so this needs to be batched, also need to have delays so we don't overwhelm stuff
