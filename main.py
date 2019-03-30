2293896c45e5c8cd215b06aac1b48f6040f21b33
Kevin Quinn <kpquinn2@wisc.edu>
2019-03-30 10:19:05 -0500

    Add checks for no results page

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-import cookielib
-import urllib
-import urllib2
-from subjects import subject_list
 
-# parsed querystring
+
+# login uses saml, which is a huge suck to do in a script i guess
+# able to login with NetID and get cookie or have user do it
+# saml is apparently more annoying to impersonate than just making a single request (at least so far)
+# https://stackoverflow.com/questions/16512965/logging-into-saml-shibboleth-authenticated-server-using-python
+# https://stackoverflow.com/questions/52618451/python-requests-saml-login-redirect
+# https://stackoverflow.com/questions/22857889/ssl-error-using-python-requests-to-access-shibboleth-authenticated-server
+
+# parsed querystring 
 # {'q': ['subject-all=[Conduct of Life] {{(publisher-date=[2000-01-01~To~2000-12-31])}}'], 'ast': ['se']}
 old = "http://www.booksinprint.com.ezproxy.library.wisc.edu/Search/Results?q=subject-all%3D[Conduct%20of%20Life]%20%7B%7B(publisher-date%3D[2000-01-01~To~2000-12-31])%7D%7D&ast=se"
 # print(parse_qs("q=subject-all%3D[Conduct%20of%20Life]%20%7B%7B(publisher-date%3D[2000-01-01~To~2000-12-31])%7D%7D&ast=se"))
@@ -16,10 +20,26 @@ old = "http://www.booksinprint.com.ezproxy.library.wisc.edu/Search/Results?q=sub
 #     #"ezproxy": "YvTB0BRT4uq2sqB"
 # }
 
-# acquire necessary cookie for access
-def login():
-    cookie = ""
-    return cookie
+
+def find_result_count(page):
+# TODO: fat file, so it takes a long time for BeautifulSoup to load it in,
+# if this needs to be run a lot would want to find a way to minimize time/downloads
+    with open("divNoresults.html", "rb") as f:
+        soup = BeautifulSoup(f, "html.parser")
+        no_results_div = soup.find("div", {"id": "divNoresults"})
+        if no_results_div:
+            print("Got no results page")
+            total = 0
+        else:
+            #example:  <span id="resultsCount">Showing 1- 25 of 889</span>
+            result_span = soup.find("span", {"id": "resultsCount"})
+            print("___________________")
+            print(result_span)
+            words = result_span.text
+            total = words[-1]
+        
+        print("Total: %d" %total)
+        return total
 
 
 def acquire_page(cookie, query):
@@ -30,81 +50,29 @@ def acquire_page(cookie, query):
         "ezproxy": cookie
     }
     resp = requests.get(base_url + path, params=query, cookies=cookies)
+    if resp.status_code > 300:
+        print("[!] failed, got resp %d" % resp.status_code)
+        return None
+
+    return resp.content
+
 
 def main():
-    # cookie = login()
 
+    # TODO: have to be able to handle AND queries and other special symbols
+    #  ['subject-all=[Children] AND subject-all=[Management] '],  same for OR
+    #
     # query = {
     #     "q": "subject-all-[%s] {{(publisher-date=[%s~To~%s])}}" %(subject, start_date, end_date),
     #     "ast": "se"
     # }
 
     # page = acquire_page(cookie, query)
+    # if not page:
+    #     raise SystemExit
 
-# fat file, so it takes a long time for BeautifulSoup to load it in,
-# if this needs to be run a lot would want to find a way to minimize time/downloads
+    find_result_count(None)
 
-    # possibly use urllib2 to get page straight from website not a file
-    # couldn't get the cookies to work ends in SSL: CERTIFICATE_VERIFY_FAILED
-    # https://stackoverflow.com/questions/13925983/login-to-website-using-urllib2-python-2-7
-"""
-    # Store the cookies and create an opener that will hold them
-    cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-
-    # Add our headers
-    opener.addheaders = [('User-agent', 'LibraryTesting')]
-
-    # Install our opener (note that this changes the global opener to the one
-    # we just made, but you can also just call opener.open() if you want)
-    urllib2.install_opener(opener)
-
-    # The action/ target from the form
-    authentication_url = 'https://account.library.wisc.edu'
-
-    # Input parameters we are going to send
-    payload = {
-    'op': 'login-main',
-    'user': '<username>',
-    'passwd': '<password>'
-    }
-
-    # Use urllib to encode the payload
-    data = urllib.urlencode(payload)
-
-    # Build our Request object (supplying 'data' makes it a POST)
-    req = urllib2.Request(authentication_url, data)
-
-    # Make the request and read the response
-    page = urllib2.urlopen(old)
-    soup = BeautifulSoup(page, "html.parser")
-    print soup
-    # <span id="resultsCount">Showing 1- 25 of 889</span>
-    result_span = soup.find("span", {"id": "resultsCount"})
-    words = result_span.text
-    words_split = words.split(" ")
-    total = words_split[-1]
-
-    print("Total: " + total)
-"""
-
-    with open("example.html", "rb") as f:
-        soup = BeautifulSoup(f, "html.parser")
-
-        # <span id="resultsCount">Showing 1- 25 of 889</span>
-        result_span = soup.find("span", {"id": "resultsCount"})
-        words = result_span.text
-        words_split = words.split(" ")
-        total = words_split[-1]
-
-        print("Total: " + total)
 
 if __name__ == "__main__":
     main()
-
-
-# able to login with NetID and get cookie or have user do it
-
-# use cookie to acquire page, check that we got what we expected
-
-# scrape the page
